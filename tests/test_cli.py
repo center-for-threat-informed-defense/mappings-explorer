0c31ac0ef22e1909187085e5147630ad0b4fb8a3
Eva <emeth.ctr@kr.af.mil>
2023-09-27 15:17:34 -0400

    fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,36 +1,36 @@
 import os
+
 import yaml
-from tests.expected_results import (
-    expected_cve_mapping,
-    expected_nist_mapping,
-    expected_security_stack_mapping,
-    expected_veris_mapping
-)
+
 from src.mappings_explorer.cli.cli import (
     configure_cve_mappings,
     configure_nist_mappings,
     configure_security_stack_mappings,
     configure_veris_mappings,
-    read_excel_file,
     read_csv_file,
+    read_excel_file,
     read_json_file,
     read_yaml,
 )
+from tests.expected_results import (
+    expected_cve_mapping,
+    expected_nist_mapping,
+    expected_security_stack_mapping,
+    expected_veris_mapping,
+)
+
 
 def test_nist_mappings_parser():
     # ARRANGE
-    filepath = os.path.join(os.path.dirname(__file__), 'files/test_nist_mappings.xlsx')
-    attack_version = '13.0'
-    mappings_version = '1'
+    filepath = os.path.join(os.path.dirname(__file__), "files/test_nist_mappings.xlsx")
+    attack_version = "13.0"
+    mappings_version = "1"
 
     # ACT
     dataframe = read_excel_file(filepath)
     parsed_mappings = []
-    parsed_mappings = configure_nist_mappings(
-        dataframe,
-        parsed_mappings,
-        attack_version,
-        mappings_version
+    configure_nist_mappings(
+        dataframe, parsed_mappings, attack_version, mappings_version
     )
     result = yaml.dump(parsed_mappings)
 
@@ -40,12 +40,14 @@ def test_nist_mappings_parser():
 
 def test_security_stack_mappings():
     # ARRANGE
-    filepath = os.path.join(os.path.dirname(__file__), 'files/test_security_stack_mappings.yaml')
+    filepath = os.path.join(
+        os.path.dirname(__file__), "files/test_security_stack_mappings.yaml"
+    )
 
     # ACT
     data = read_yaml(filepath)
     parsed_mappings = []
-    parsed_mappings = configure_security_stack_mappings(data, parsed_mappings)
+    configure_security_stack_mappings(data, parsed_mappings)
     result = yaml.dump(parsed_mappings)
 
     # ASSERT
@@ -54,12 +56,12 @@ def test_security_stack_mappings():
 
 def test_veris_mappings():
     # ARRANGE
-    filepath = os.path.join(os.path.dirname(__file__), 'files/test_veris_mappings.json')
+    filepath = os.path.join(os.path.dirname(__file__), "files/test_veris_mappings.json")
     parsed_mappings = []
 
     # ACT
     veris_mappings = read_json_file(filepath)
-    parsed_mappings = configure_veris_mappings(veris_mappings, parsed_mappings)
+    configure_veris_mappings(veris_mappings, parsed_mappings)
     result = yaml.dump(parsed_mappings)
 
     # ASSERT
@@ -68,12 +70,12 @@ def test_veris_mappings():
 
 def test_cve_mappings():
     # ARRANGE
-    filepath = os.path.join(os.path.dirname(__file__), 'files/test_cve_mappings.csv')
+    filepath = os.path.join(os.path.dirname(__file__), "files/test_cve_mappings.csv")
     id_to_name_dict = {
-        'T1059': 'Name for T1059',
-        'T1190': 'Name for T1190',
-        'T1078': 'Name for T1078',
-        'T1068': 'Name for T1068',
+        "T1059": "Name for T1059",
+        "T1190": "Name for T1190",
+        "T1078": "Name for T1078",
+        "T1068": "Name for T1068",
     }
 
     # ACT
