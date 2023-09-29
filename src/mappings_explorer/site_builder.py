2bc01f512919dfe3df59e89c63ffdf61e4e17c4a
arobbins <arobbins@mitre.org>
2023-09-29 14:51:07 -0400

    add external controls landing page

--- src/mappings_explorer/site_builder.py
+++ src/mappings_explorer/site_builder.py
@@ -2,15 +2,22 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def main():
-    print("Creating site index")
     templateLoader = FileSystemLoader(searchpath="./templates")
     templateEnv = Environment(loader=templateLoader)
     TEMPLATE_FILE = "landing.html.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
-
     template.stream(
         title="Mappings Explorer"
     ).dump('./output/index.html')
+    print("Created site index")
+
+    TEMPLATE_FILE = "external-landing.html.j2"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    template.stream(
+        title="External Mappings Home"
+    ).dump('./output/external-landing.html')
+    print("Created external mappings home")
 
 if __name__ == "__main__":
     main()
