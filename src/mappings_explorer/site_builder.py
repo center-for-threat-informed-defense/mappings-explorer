0bb656764d7a653ee6173b4a0e61124b15db6d8e
Allison Robbins <allisonrobbins@gwu.edu>
2023-10-04 21:22:51 -0400

    feat: implement initial Mappings Explorer

---
+++ src/mappings_explorer/site_builder.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+def main():
+    print("Creating site index")
+    templateLoader = FileSystemLoader(searchpath="./templates")
+    templateEnv = Environment(loader=templateLoader, autoescape=True)
+    TEMPLATE_FILE = "landing.html.j2"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+    template.stream(title="Mappings Explorer").dump("./output/index.html")
+
+
+if __name__ == "__main__":
+    main()
