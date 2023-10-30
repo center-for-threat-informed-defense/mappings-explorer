33c387a7925bb8c537f16716cb8badd3c5d685f5
Eva <emeth.ctr@kr.af.mil>
2023-10-30 09:00:07 -0400

    fix tests

--- tests/test_mapex_cli.py
+++ tests/test_mapex_cli.py
@@ -3,7 +3,6 @@ import os
 
 from mapex.cli import read_json_file
 from mapex.write_parsed_mappings import (
-    get_filename_version_string,
     write_parsed_mappings_csv,
     write_parsed_mappings_navigator_layer,
     write_parsed_mappings_stix,
@@ -23,11 +22,11 @@ def test_write_mappings_to_yaml(tmpdir):
         os.path.dirname(__file__), "files/parsed_mappings.json"
     )
     parsed_mappings = read_json_file(json_filepath)
-    filepath = f"{tmpdir}"
+    filepath = f"{tmpdir}/parsed_mappings"
 
     # ACT
     write_parsed_mappings_yaml(parsed_mappings, filepath)
-    file = open(f"{filepath}_attack-13.0.yaml", "r", encoding="UTF-8")
+    file = open(f"{filepath}.yaml", "r", encoding="UTF-8")
     result = file.read()
 
     # ASSERT
@@ -40,7 +39,7 @@ def test_write_mappings_to_csv(tmpdir):
     root_dir = os.path.dirname(__file__)
     json_filepath = os.path.join(root_dir, "files/parsed_mappings.json")
     parsed_mappings = read_json_file(json_filepath)
-    filepath = f"{tmpdir}"
+    filepath = f"{tmpdir}/parsed_mappings"
     expected_attack_objects_file = open(
         f"{root_dir}/expected_results/expected_csv_results_attack_objects.csv",
         "r",
@@ -55,15 +54,12 @@ def test_write_mappings_to_csv(tmpdir):
 
     # ACT
     write_parsed_mappings_csv(parsed_mappings, filepath, metadata_key)
-    version_string = get_filename_version_string(parsed_mappings)
     attack_objects_file = open(
-        f"{filepath}{version_string}_attack_objects.csv",
+        f"{filepath}_attack_objects.csv",
         "r",
         encoding="UTF-8",
     )
-    metadata_file = open(
-        f"{filepath}{version_string}_metadata.csv", "r", encoding="UTF-8"
-    )
+    metadata_file = open(f"{filepath}_metadata.csv", "r", encoding="UTF-8")
 
     # ASSERT
     assert expected_attack_objects_file.read() == attack_objects_file.read()
@@ -75,14 +71,11 @@ def test_write_mappings_to_navigator_layer(tmpdir):
     root_dir = os.path.dirname(__file__)
     json_filepath = os.path.join(root_dir, "files/parsed_mappings.json")
     parsed_mappings = read_json_file(json_filepath)
-    filepath = f"{tmpdir}"
+    filepath = f"{tmpdir}/parsed_mappings"
 
     # ACT
     write_parsed_mappings_navigator_layer(parsed_mappings, filepath)
-    version_string = get_filename_version_string(parsed_mappings)
-    file = open(
-        f"{filepath}{version_string}_navigator_layer.json", "r", encoding="UTF-8"
-    )
+    file = open(f"{filepath}_navigator_layer.json", "r", encoding="UTF-8")
     result = json.load(file)
 
     # ASSERT
@@ -94,12 +87,11 @@ def test_write_mappings_to_stix(tmpdir):
     root_dir = os.path.dirname(__file__)
     json_filepath = os.path.join(root_dir, "files/parsed_mappings.json")
     parsed_mappings = read_json_file(json_filepath)
-    filepath = f"{tmpdir}"
+    filepath = f"{tmpdir}/parsed_mappings"
 
     # ACT
     write_parsed_mappings_stix(parsed_mappings, filepath)
-    version_string = get_filename_version_string(parsed_mappings)
-    file = open(f"{filepath}{version_string}_stix.json", "r", encoding="UTF-8")
+    file = open(f"{filepath}_stix.json", "r", encoding="UTF-8")
     result = json.load(file)
 
     # ASSERT
