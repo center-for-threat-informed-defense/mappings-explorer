07b379a9b063d11fa3b7b1c67492fe4f23977d1d
Eva <emeth.ctr@kr.af.mil>
2023-11-29 12:45:22 -0500

    add json builder for coverage for matrix

--- src/mappings_explorer/attack_query.py
+++ src/mappings_explorer/attack_query.py
@@ -19,7 +19,7 @@ def load_attack_json(attack_version, attack_domain):
 
 
 def build_attack_dict(attack_data):
-    attack_data_dict = {}
+    attack_data_array = []
     for attack_object in attack_data["objects"]:
         # skip objects without IDs
         if not attack_object.get("external_references"):
@@ -37,10 +37,36 @@ def build_attack_dict(attack_data):
             attack_object_name = attack_object.get("name")
             attack_object_url = external_references[0].get("url")
             attack_object_description = attack_object.get("description")
-            attack_data_dict[attack_object_id] = {
-                "name": attack_object_name,
-                "url": attack_object_url,
-                "description": attack_object_description,
-            }
+            attack_object_short_name = attack_object.get("x_mitre_shortname", "")
+            attack_object_type = (
+                "tactic"
+                if attack_object.get("type") == "x-mitre-tactic"
+                else "subtechnique"
+                if attack_object.get("x_mitre_is_subtechnique")
+                else "technique"
+            )
+            attack_object_parents = []
+            if attack_object_type == "technique":
+                kill_chain_phases = attack_object.get("kill_chain_phases", [])
+                for phase in kill_chain_phases:
+                    if phase.get("kill_chain_name") == "mitre-attack":
+                        attack_object_parents.append(phase.get("phase_name"))
+            attack_object_technique = (
+                attack_object_id[0 : attack_object_id.index(".")]
+                if attack_object_type == "subtechnique"
+                else ""
+            )
+            attack_data_array.append(
+                {
+                    "id": attack_object_id,
+                    "name": attack_object_name,
+                    "url": attack_object_url,
+                    "description": attack_object_description,
+                    "type": attack_object_type,
+                    "tactics": attack_object_parents,
+                    "technique": attack_object_technique,
+                    "short_name": attack_object_short_name,
+                }
+            )
 
     return attack_data_dict
