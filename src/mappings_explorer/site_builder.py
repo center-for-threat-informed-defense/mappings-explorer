3606b7a7db64e2b03940fbc916dab7eda981f0ba
Allison Robbins <allisonrobbins@gwu.edu>
2023-12-19 16:05:24 -0500

    Mapex 85 add capability pages (#28)
    * build base for generating website
    
    * build landing page from template
    
    * generate index page
    
    * generate a page for each capability
    
    * fix linking in tables
    
    * add descriptions for cve and security stack
    
    * get nist descriptions
    
    * fix lint
    
    * fix case sensitive path names
    
    * uncomment nist and cve now links are fixed
    
    ---------
    
    Co-authored-by: arobbins <arobbins@mitre.org>
    Co-authored-by: Mark E. Haase <mhaase@mitre.org>

--- src/mappings_explorer/site_builder.py
+++ src/mappings_explorer/site_builder.py
@@ -1,11 +1,23 @@
 import argparse
 import json
+import os
 import shutil
 
+import requests
 from jinja2 import Environment, FileSystemLoader
+from mapex_convert.read_files import (
+    read_yaml_file,
+)
 
 from .attack_query import create_attack_jsons
-from .template import PUBLIC_DIR, ROOT_DIR, TEMPLATE_DIR, load_template
+from .template import DATA_DIR, PUBLIC_DIR, ROOT_DIR, TEMPLATE_DIR, load_template
+
+
+class Capability:
+    id = ""
+    label = ""
+    description = ""
+    mappings = []
 
 
 class ExternalControl:
@@ -19,9 +31,9 @@ class ExternalControl:
     attackDomain = ""
     attackDomains = []
     validVersions = []
-    tableHeaders = []
     groups = []
     mappings = []
+    capabilities = []
 
 
 def load_projects():
@@ -62,7 +74,6 @@ def load_projects():
     ]
     nist.attackDomains = ["enterprise"]
     nist.attackDomain = nist.attackDomains[0]
-    nist.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     veris = ExternalControl()
     veris.id = "veris"
     veris.label = "VERIS"
@@ -81,7 +92,6 @@ def load_projects():
         "9.0",
     ]
     veris.validVersions = [("1.3.5", "9.0"), ("1.3.7", "12.1")]
-    veris.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     veris.mappings = []
 
     cve = ExternalControl()
@@ -102,7 +112,6 @@ def load_projects():
     cve.versions = ["10.21.2021"]
     cve.attackVersions = ["9.0"]
     cve.validVersions = [("10.21.2021", "9.0")]
-    cve.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     cve.mappings = []
 
     aws = ExternalControl()
@@ -122,7 +131,6 @@ def load_projects():
     aws.attackVersions = ["9.0"]
     aws.versions = ["09.21.2021"]
     aws.validVersions = [("09.21.2021", "9.0")]
-    aws.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     aws.mappings = []
 
     azure = ExternalControl()
@@ -142,7 +150,6 @@ def load_projects():
     azure.attackVersions = ["8.2"]
     azure.versions = ["06.29.2021"]
     azure.validVersions = [("06.29.2021", "8.2")]
-    azure.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     azure.mappings = []
 
     gcp = ExternalControl()
@@ -163,7 +170,6 @@ def load_projects():
     gcp.attackVersion = gcp.attackVersions[0]
     gcp.versions = ["06.28.2022"]
     gcp.validVersions = [("06.28.2022", "10.0")]
-    gcp.tableHeaders = ["ID", "Control Family", "Number of Controls", "Description"]
     gcp.mappings = []
 
     projects = [
@@ -227,6 +233,84 @@ def parse_groups(project, attack_version, project_version):
             + " mappings in group: "
             + project_group["name"]
         )
+    project.capabilities = parse_capabilities(project)
+    #  set the descriptions for each project's capability list
+    if project.id == "cve":
+        get_cve_descriptions(project=project)
+    if project.id == "nist":
+        get_nist_descriptions(project=project, version=project_version)
+    if project.id == "aws" or project.id == "gcp" or project.id == "azure":
+        get_security_stack_descriptions(project=project)
+
+
+def get_security_stack_descriptions(project):
+    root = DATA_DIR / "SecurityStack"
+    data_dir = os.listdir(root)
+    for dir in data_dir:
+        if dir.lower() == project.id:
+            rootdir = root / dir
+
+    # iterate through mappings files
+    for file in os.listdir(rootdir):
+        data = read_yaml_file(rootdir / file)
+        name = data["name"]
+        description = data["description"]
+        for c in project.capabilities:
+            if c.id.lower().replace(" ", "_") == name.lower().replace(" ", "_"):
+                c.description = description
+                c.label = data["name"]
+                break
+
+
+def get_cve_descriptions(project):
+    for c in project.capabilities:
+        try:
+            response = requests.get("https://cveawg.mitre.org/api/cve/" + c.id).json()
+            descriptions = response["containers"]["cna"]["descriptions"]
+            c.description = descriptions[0]["value"]
+        except Exception:
+            c.description = ""
+
+
+def get_nist_descriptions(project, version):
+    rev5_link = "https://csrc.nist.gov/extensions/nudp/services/json/nudp/framework/version/sp_800_53_5_1_1/element/"
+    rev4_link = "https://csrc.nist.gov/extensions/nudp/services/json/nudp/framework/version/sp_800_53_4_0_0/element/"
+    link = ""
+    if version == "rev4":
+        link = rev4_link
+    else:
+        link = rev5_link
+
+    for c in project.capabilities:
+        try:
+            id = c.id
+            if len(id) < 5 and version != "rev4":
+                id = c.id[0:3] + "0" + c.id[3:4]
+            response = requests.get(link + id + "/graph").json()
+            elements = response["response"]["elements"]
+            element_array = elements[0]["elements"][0]["elements"]
+            for item in element_array:
+                if item["elementTypeIdentifier"] == "discussion":
+                    c.description = item["text"].replace("<p>", "").replace("</p>", "")
+                    break
+        except Exception as e:
+            print("exception ", e)
+
+
+def parse_capabilities(project):
+    mappings = project.mappings
+    allIds = [m["capability_id"] for m in mappings]
+    capabilityIds = list(set(allIds))
+    capabilities = []
+    for id in capabilityIds:
+        c = Capability()
+        c.id = id
+        c.mappings = [m for m in mappings if (m["capability_id"] == id)]
+        print(
+            "for capability " + c.id + " number of mappings is  " + str(len(c.mappings))
+        )
+        capabilities.append(c)
+    return capabilities
 
 
 def build_external_landing(
@@ -239,27 +323,49 @@ def build_external_landing(
 ):
     output_path = project_dir / "index.html"
     template = load_template("external-control.html.j2")
-
+    attack_prefix = url_prefix + "attack/" + "attack-" + attack_version + "/"
+    external_prefix = (
+        url_prefix
+        + "external/"
+        + project.id
+        + "/attack-"
+        + attack_version
+        + "/"
+        + project.id
+        + "-"
+        + project_version
+        + "/"
+    )
     headers = [
-        ("attack_object_id", "ATT&CK ID"),
-        ("attack_object_name", "ATT&CK Name"),
+        ("attack_object_id", "ATT&CK ID", "attack_object_id", attack_prefix),
+        ("attack_object_name", "ATT&CK Name", "attack_object_id", attack_prefix),
         ("mapping_type", "Mapping Type"),
-        ("capability_id", "Capability ID"),
-        ("capability_description", "Capability Description"),
+        ("capability_id", "Capability ID", "capability_id", external_prefix),
+        (
+            "capability_description",
+            "Capability Description",
+            "capability_id",
+            external_prefix,
+        ),
     ]
     if project.id == "azure" or project.id == "aws" or project.id == "gcp":
         headers = [
-            ("attack_object_id", "ATT&CK ID"),
-            ("attack_object_name", "ATT&CK Name"),
+            ("attack_object_id", "ATT&CK ID", "attack_object_id", attack_prefix),
+            ("attack_object_name", "ATT&CK Name", "attack_object_id", attack_prefix),
             ("score_category", "Category"),
             ("score_value", "Value"),
-            ("capability_id", "Capability ID"),
-            ("capability_description", "Capability Description"),
+            ("capability_id", "Capability ID", "capability_id", external_prefix),
+            (
+                "capability_description",
+                "Capability Description",
+                "capability_id",
+                external_prefix,
+            ),
         ]
 
     group_headers = [
-        ("id", "ID"),
-        ("name", "Control Family"),
+        ("id", "ID", "id", external_prefix),
+        ("name", "Control Family", "id", external_prefix),
         # ("num_controls", "Number of Controls"),
         ("num_mappings", "Number of Mappings"),
     ]
@@ -278,7 +384,6 @@ def build_external_landing(
         attackVersions=project.attackVersions,
         domain=project.attackDomain,
         domains=project.attackDomains,
-        tableHeaders=project.tableHeaders,
         mappings=mappings,
         headers=headers,
         group_headers=group_headers,
@@ -302,8 +407,17 @@ def build_external_landing(
             parent_dir=project_dir,
             project_version=project_version,
             attack_version=attack_version,
-            mappings=mappings,
             headers=headers,
+        )
+    for capability in project.capabilities:
+        build_external_capability(
+            project=project,
+            url_prefix=url_prefix,
+            parent_dir=project_dir,
+            project_version=project_version,
+            attack_version=attack_version,
+            headers=headers,
+            capability=capability,
         )
 
 
@@ -353,7 +467,6 @@ def build_external_control(
     parent_dir,
     project_version,
     attack_version,
-    mappings,
     headers,
 ):
     group_id = group["id"]
@@ -371,7 +484,6 @@ def build_external_control(
         group_name=group_name,
         project=project,
         description=project.description,
-        tableHeaders=project.tableHeaders,
         control_version=project_version,
         versions=project.versions,
         attack_version=attack_version,
@@ -384,6 +496,42 @@ def build_external_control(
     )
     stream.dump(str(output_path))
     print("          Created group page " + group_name)
+
+
+def build_external_capability(
+    project: ExternalControl,
+    url_prefix,
+    parent_dir,
+    project_version,
+    attack_version,
+    headers,
+    capability,
+):
+    dir = parent_dir / capability.id
+    dir.mkdir(parents=True, exist_ok=True)
+    output_path = dir / "index.html"
+    template = load_template("external-capability.html.j2")
+    prev_page = parent_dir
+    stream = template.stream(
+        title=project.label + " " + capability.id,
+        url_prefix=url_prefix,
+        control=project.label,
+        project=project,
+        project_id=project.id,
+        description=capability.description,
+        control_version=project_version,
+        versions=project.versions,
+        attack_version=attack_version,
+        attackVersions=project.attackVersions,
+        domain=project.attackDomain,
+        domains=project.attackDomains,
+        prev_page=prev_page,
+        mappings=capability.mappings,
+        headers=headers,
+        capability=capability,
+    )
+    stream.dump(str(output_path))
+    print("          Created capability page " + capability.id)
 
 
 def build_matrix(url_prefix):
