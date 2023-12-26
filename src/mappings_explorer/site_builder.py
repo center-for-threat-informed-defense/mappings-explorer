09635bff17eed6be3181043f9d02ba0eb5d9ccaa
Eva <emeth.ctr@kr.af.mil>
2023-12-26 15:16:32 -0500

    uncomment code that was commented out for testing purposes

--- src/mappings_explorer/site_builder.py
+++ src/mappings_explorer/site_builder.py
@@ -790,11 +790,11 @@ def main():
     TEMPLATE_FILE = "external-control.html.j2"
     template = templateEnv.get_template(TEMPLATE_FILE)
 
-    # build_external_pages(projects=projects, url_prefix=url_prefix)
-    # build_matrix(url_prefix)
-    # template.stream(title="External Mappings Home").dump(
-    #     "./output/external-landing.html"
-    # )
+    build_external_pages(projects=projects, url_prefix=url_prefix)
+    build_matrix(url_prefix)
+    template.stream(title="External Mappings Home").dump(
+        "./output/external-landing.html"
+    )
     print("Created external mappings home")
 
     build_search_index(url_prefix)
