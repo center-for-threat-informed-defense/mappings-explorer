578b7e97b5592faf39a63a9eb2b384be88051e03
arobbins <arobbins@mitre.org>
2024-01-12 14:32:33 -0500

    add jinja-markdown extension

--- src/mappings_explorer/template.py
+++ src/mappings_explorer/template.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, Template
+from jinja_markdown import MarkdownExtension
 
 ROOT_DIR = Path(__file__).parents[2]
 PUBLIC_DIR = ROOT_DIR / "output"
@@ -8,6 +9,7 @@ TEMPLATE_DIR = ROOT_DIR / "src" / "mappings_explorer" / "templates"
 DATA_DIR = ROOT_DIR / "src" / "mapex_convert" / "mappings"
 
 _environment = Environment(loader=FileSystemLoader(TEMPLATE_DIR), autoescape=True)
+_environment.add_extension(MarkdownExtension)
 
 
 def load_template(name: str) -> Template:
