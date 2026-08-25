5eee5098b7a516288d7f3b3d5e6bcdcf2da97802
arobbins <arobbins@mitre.org>
2026-08-25 12:35:26 -0400

    update with description from Tiffany

--- src/mappings_explorer/framework_setup.py
+++ src/mappings_explorer/framework_setup.py
@@ -440,15 +440,23 @@ def load_projects():
 
     cis = ExternalControl()
     cis.id = "cis"
-    cis.label = "CIS"
-    cis.description = """description goes here."""
+    cis.label = "CIS Controls"
+    cis.description = """The CIS Critical Security Controls® (CIS Controls®) are a
+     prioritized set of actions that collectively form a defense-in-depth set of best
+     practices that mitigate the most common attacks against systems and networks. These
+     mappings provide resources for assessing effective technical defenses against
+     actual real-world threats as described in the MITRE ATT&CK® knowledge base and
+     provide a foundation for integrating ATT&CK-based threat intelligence into the
+     process. The connection of ATT&CK with the CIS Controls empowers threat-informed
+     analysis and decision-making for cybersecurity control program design and
+     implementation. """
     cis.attackDomains = ["Enterprise"]
     cis.attackDomain = csa_ccm.attackDomains[0]
     cis.attackVersions = ["19.1"]
     cis.attackVersion = csa_ccm.attackVersions[0]
-    cis.versions = ["8.2"]
+    cis.versions = ["8.1"]
     cis.validVersions = [
-        ("8.2", "19.1", "Enterprise"),
+        ("8.1", "19.1", "Enterprise"),
     ]
     cis.mappings = []
     cis.resources = [
@@ -461,8 +469,8 @@ def load_projects():
             "label": "Mapping Scope",
         },
         {
-            "link": "https://www.cisecurity.org/controls",
-            "label": "CIS (External link)",
+            "link": "https://cas.docs.cisecurity.org/en/latest/source/About%20the%20CIS%20Controls/",
+            "label": "CIS Controls (External link)",
             "external": True,
         },
     ]
